734d4d4489611a64bbd0cc88ee000c82b4892717
nngel <edwardangellorica@gmail.com>
2025-02-17 00:09:31 +0800

    qr

--- screens/QRScreen/QRScanScreen.tsx
+++ screens/QRScreen/QRScanScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import { Camera, CameraView } from "expo-camera";
 import {
   AppState,
@@ -9,7 +9,12 @@ import {
   StatusBar,
   StyleSheet,
 } from "react-native";
-import { Overlay } from "./Overlay";
+
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+
 import { useEffect, useRef } from "react";
 
 export default function Home() {
@@ -42,13 +47,56 @@ export default function Home() {
         facing="back"
         onBarcodeScanned={({ data }) => {
           if (data && !qrLock.current) {
-            qrLock.current = true;
             setQRData(data);
           }
         }}
       />
-      <Text>Read: {QRData}</Text>
-      <Overlay />
+      <View style={styles.Overlay}>
+        <View style={styles.Top}></View>
+        <View style={styles.Middle} />
+        <View style={styles.Bottom} />
+      </View>
+      <Text style={styles.texttest}>Read: {QRData}</Text>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  Overlay: {
+    position: "absolute",
+    width: wp(100),
+    height: hp(100),
+  },
+  Top: {
+    position: "absolute",
+    width: wp(100),
+    height: hp(25),
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  Middle: {
+    position: "absolute",
+    top: hp(25),
+    width: wp(100),
+    height: wp(100) - wp(20),
+    borderLeftWidth: wp(10),
+    borderRightWidth: wp(10),
+    borderColor: "rgba(0,0,0,0.5)",
+  },
+  Bottom: {
+    position: "absolute",
+    top: hp(25) + wp(100) - wp(20),
+    width: wp(100),
+    height: hp(100) - (wp(100) - wp(20)),
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  texttest: {
+    position: "absolute",
+    width: wp(100),
+    bottom: 0,
+    left: 0,
+    margin: 5,
+    marginBottom: hp(5),
+    color: "#FFF",
+    textAlign: "center",
+  },
+});
